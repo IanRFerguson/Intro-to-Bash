c4e3ad21188f834dc9ab34c4a9115f277a7c41bc
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-11-13 07:43:10 -0800

    Create 09b_bash-scripting.sh

---
+++ 09b_bash-scripting.sh
@@ -0,0 +1,40 @@
+#!/usr/env/bin bash
+
+# ----------------- Navigate directory
+
+target="08_Bash-Scripting-Practice/"
+cd $target
+
+# ----------------- Count local files
+
+file_count=0
+
+for file in *
+do
+	if [ -f $file ]
+	then
+		file_count=$((file_count+1))
+	fi
+done
+
+echo "$file_count files detected"
+
+# ------------------ Move files to proper directories
+
+echo "Moving Python files"
+mv *.py python-scripts/
+
+echo "Moving C files"
+mv *.c c-scripts/
+
+echo "Moving JS files"
+mv *.js javascripts/
+
+# ------------------ Add Python shebang
+
+for file in python-scripts/*;
+do
+	#echo $file
+	echo "Amending $file with Python shebang"
+	echo '#!/usr/bin/env python' >> $file
+done
