3922b74f56a5eaef5846a67aed1a3fd0de284bf6
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-11-12 20:07:19 -0800

    Create 06_conditional-logic.sh

---
+++ 06_conditional-logic.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# ---------------- If / Else conditional logic
+
+# Start logic block with if [ check ] then
+# Always include else block
+# Finish logic block with fi
+
+myTeam = "NYK"
+
+if [ $myTeam == "NYK" ]
+then
+	echo "We need a lot of help"
+else
+	echo "We might have a chance!"
+fi
+
+
+# ---------------- Evaluative operators
+
+# These are pretty different from other programming languages
+
+-eq		# Equal (use for numeric)
+-ne		# Not equal (use for numeric)
+-le		# Less than or equal
+-lt		# Less than
+-ge		# Greater than or equal
+-gt		# Greater than
+-z		# Is null
+==		# Equal (use for strings)
+!=		# Not equal (use for strings)
+
+
+magicNumber = 8
+yourNumber = 6
+
+if [ $yourNumber -lt $magicNumber ]
+then
+	echo "$yourNumber is less than $magicNumber"
+	mkdir numberDirectory/
+	echo "I made a new directory"
+else
+	echo "I dont't do anything :("
+fi
