b747f8c106742dec5f63c350c9311987eeb7d2e8
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-11-13 07:44:08 -0800

    Create 09a_practice-setup.sh

---
+++ 09a_practice-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd 08_Bash-Scripting-Practice/
+
+rm -r *
+
+touch script1.py script2.py script3.c script4.c script5.js script6.js
+
+mkdir c-scripts javascripts python-scripts
+
+# done!
