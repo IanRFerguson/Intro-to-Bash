500b6b241470ebb994a0424766a86a99a2cda8fe
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-11-15 14:45:27 -0500

    Update 07_loops.sh

--- 07_loops.sh
+++ 07_loops.sh
@@ -4,21 +4,32 @@
 
 # Like any other language, for every item in a list/vector/array/etc
 
-for player in $roster
-do
+# -------- Python-style For Loop
+
+roster=('Steph' 'Lebron' 'Kawhi' 'Giannis')
+
+for player in "${roster[@]}"; do	# Notice the array syntax here
 	echo "$player made the team"
 done
 
+# -------- C-style For Loop
+
+celtics=("Jayson" "Jaylon" "Kemba" "Enes" "Gordon")
+
+for ((i=0; i<5; i++)); do
+	echo ${celtics[$i]}
+done
 
 # ------------- While Loops
 
 # Loop executes as long as a condition is met
 
-while [ $index -ne 10 ]
-do
-	echo "Current index: $index"
-	index=$((index+1))			# Crucial to iterate in while loops!
-done						# Avoid infinite loops
+index=0
+
+while [ $index -ne 10 ]; do
+	echo "Current index: $index"		# Crucial to iterate in while loops!
+	index=$((index+1))							# Avoid infinite loops
+done
 
 
 # ------------- Until Loops
@@ -26,10 +37,11 @@ done						# Avoid infinite loops
 # Like a reverse while loop...
 # Executes while a condition is NOT met
 
-until [ $index -eq 15 ]
-do
+index=10
+
+until [ $index -eq 15 ]; do
 	echo "I only have $index plants!"
-	index=(($index+1))
+	index=$((index+1))
 done
 
 echo "I have enough plants now"
