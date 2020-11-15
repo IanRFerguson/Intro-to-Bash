447d4e2698da5a653d79dde8c7bb51e35e4760ef
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-11-15 14:48:52 -0500

    Update 06_conditional-logic.sh

--- 06_conditional-logic.sh
+++ 06_conditional-logic.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
-# ---------------- If / Else conditional logic
+# ---------------- Conditional Logic
 
 # Start logic block with if [ check ] then
 # Always include else block
 # Finish logic block with fi
 
-myTeam = "NYK"
+myTeam='NYK'
 
-if [ $myTeam == "NYK" ]
-then
+if [ $myTeam == "NYK" ]; then
 	echo "We need a lot of help"
 else
 	echo "We might have a chance!"
+fi
+
+# If / Elif / Else
+
+if [[ -z $myTeam ]]; then
+	echo "myTeam is empty"
+elif [[ -d $myTeam ]]; then
+	echo "myTeam is a directory"
+else
+	echo "myTeam is the $myTeam"
 fi
 
 
@@ -20,16 +29,17 @@ fi
 
 # These are pretty different from other programming languages
 
--eq		# Equal (use for numeric)
--ne		# Not equal (use for numeric)
--le		# Less than or equal
--lt		# Less than
--ge		# Greater than or equal
--gt		# Greater than
--z		# Is null
-==		# Equal (use for strings)
-!=		# Not equal (use for strings)
+# -eq		# Equal (use for numeric)
+# -ne		# Not equal (use for numeric)
+# -le		# Less than or equal
+# -lt		# Less than
+# -ge		# Greater than or equal
+# -gt		# Greater than
+# -z		# Is null
+# ==		# Equal (use for strings)
+# !=		# Not equal (use for strings)
 
+# ---------------
 
 magicNumber = 8
 yourNumber = 6
